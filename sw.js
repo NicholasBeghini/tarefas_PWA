b4b910834786d712fff624d06dd9310f825fc1b0
Nicholas <nicholasbe77@gmail.com>
2025-08-06 15:50:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-task-list-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/tarefas_PWA/',
+    '/tarefas_PWA/index.html',
+    '/tarefas_PWA/style.css',
+    '/tarefas_PWA/manifest.json',
+    '/tarefas_PWA/icons/icon-192x192.png',
+    '/tarefas_PWA/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -25,3 +25,4 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+
